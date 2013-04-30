9a21e54d963bfc653cda94edccd694de7654d8c8
rudder-os <support@rudder-tech.com>
2013-04-30 18:55:54 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facileguides::Application.config.secret_token = 'e6c613f660bd4431edc16d39f074766c717cfdfb0102c6e4f6928c5b26d5976a431c4fcd6816a2ad4b32954ff9f2164a01da1973d86b104c62a67cdb747a3378'
